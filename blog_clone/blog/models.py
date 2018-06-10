fe0fb599fe2e2f57d4101c449b1abdae64d8bd39
sadrok <sadrok.za@gmail.com>
2018-06-10 19:01:28 +0200

    end part two - post & comment models

--- blog_clone/blog/models.py
+++ blog_clone/blog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    @property
+    def approved_comments(self):
+        return self.comments.filter(approved=True)
+
+    def get_absolute_url(self):
+        '''
+        :return: The url of this Post
+        '''
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.models.Post', related_name='comments')
+    # author may be anonymous
+    author = models.CharField(max_length=200, blank=True, null=True)
+    # and emails are required but not shown on site
+    author_email = models.EmailField()
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    approved = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved = True
+        self.save()
+
+    def get_absolute_url(self):
+        '''
+        TODO: Creating a comment will be an AJAX function soon
+        :return: The url of the post commented on
+        '''
+        return reverse('post_detail', kwargs={'pk': self.post.pk})
+
+    def __str__(self):
+        return self.text
