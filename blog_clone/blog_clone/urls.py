89c52a9ae8c1d4a2b1d07b38763e921fcb4b2d64
sadrok <sadrok.za@gmail.com>
2018-06-10 21:53:25 +0200

    end part six - login page

--- blog_clone/blog_clone/urls.py
+++ blog_clone/blog_clone/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', include('blog.urls'), name='blog'),
     path('admin/', admin.site.urls),
+    path('accounts/login', auth_views.login, name='login'),
+    path('accounts/logout', auth_views.logout, name='logout',
+         kwargs={'next': '/'}),
 ]
